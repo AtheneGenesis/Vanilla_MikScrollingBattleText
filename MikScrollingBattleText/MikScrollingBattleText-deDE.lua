e330a44e052e4b7447da91a3e9579b95380a6df7
Athene <athenelulz@gmail.com>
2015-08-27 14:58:00 +0200

    German Translations

---
+++ MikScrollingBattleText/MikScrollingBattleText-deDE.lua
@@ -0,0 +1,25 @@
+local L = AceLibrary("AceLocale-2.2"):new("MikScrollingBattleText")
+
+L:RegisterTranslations("deDE", function()
+    return {
+		["Debug mode has been enabled."] = true,
+		["Debug mode has been disabled."] = true,
+		["Event search mode has been enabled.  Searching for: "] = true,
+		["Event search mode has been disabled."] = true,
+		["The mod is now disabled."] = true,
+		["The mod is now enabled."] = true,
+		["Hits"] = "Treffer",
+		["Crit"] = "Krit",
+		["Crits"] = "Krits",
+		["Multiple"] = "Mehrere",
+		[" (%d vulnerability)"] = true,
+		[" <\124cff00b37e\124h%d\124h\124r>"] = true,
+		["Profile Reset"] = true,
+		["Drowning"] = true,
+		["Falling"] = true,
+		["Fatigue"] = true,
+		["Fire"] = true,
+		["Lava"] = true,
+		["Slime"] = true,
+    }
+end)
